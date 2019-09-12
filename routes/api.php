500de470cca09a0572251896d2f320affa996763
Iustin G <iusting20@gmail.com>
2019-09-12 11:46:15 +0000

    created API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use GuzzleHttp\GuzzleHttp;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,33 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/currency', function (Request $request) {
+    // INIT VARS
+    $initialCurrency = $request->from;
+    $targetCurrency = $request->to;
+    $amount = $request->amount;
+    $convertedAmount = null;
+
+
+    // GET CONVERSIONS
+    $contents = file_get_contents('http://www.floatrates.com/daily/'. strtolower($initialCurrency) .'.xml');
+    $xml = simplexml_load_string($contents);
+    $conversions = json_decode(json_encode($xml), true);
+    $nrOfConversions = count($conversions['item']);
+
+
+    // FIND TARGET CURRENCY    AND THEN    CONVERT AMOUNT
+    for( $i = 0; $i < $nrOfConversions; $i++)   {
+        
+        if( $conversions['item'][$i]["targetCurrency"] == $targetCurrency ) {
+            $exchangeRate = $conversions['item'][$i]['exchangeRate'];
+            $convertedAmount = $amount * $exchangeRate;
+            break;
+        }
+    }
+
+    return $convertedAmount;
 });
