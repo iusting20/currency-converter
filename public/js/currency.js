6ac0d83875feb14cd709a0ff25f013c40422841f
Iustin G <iusting20@gmail.com>
2019-09-13 00:01:09 +0000

    added AJAX and improved UI

---
+++ public/js/currency.js
@@ -0,0 +1,20 @@
+function makeConversion()  {
+
+    if( $('#convert-amount').val() == ""  ||  $('#convert-from').val()[0] == undefined   ||  $('#convert-to').val()[0] == undefined)    {
+        alert( "Fill in all fields, please !" )
+    }
+    else if( $('#convert-from').val()[0] == $('#convert-to').val()[0] )  {
+        alert( "Initial and target currencies cannot be the same !" )
+    }
+    else   {
+        $.ajax({
+            url: "https://justinsrv.xyz/_DEV_SPACE/currency-converter/public/api/currency?from=" + $('#convert-from').val()[0] + "&to=" + $('#convert-to').val()[0] + "&amount=" + $('#convert-amount').val(),
+            method : 'GET'
+        })
+        .done( function( data ) {
+            $('#display-box').css('height', '75px')
+            $('#display-box').css('opacity', '1')
+            $('#display-box').text( $('#convert-amount').val() + ' ' + $('#convert-from').val()[0] + ' = ' + data + ' ' + $('#convert-to').val()[0])
+        })
+    }
+}
